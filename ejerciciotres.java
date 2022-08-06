f63187cd3301830beed9fcbc67bbb29c947689f0
DaNyPC <106661035+DaNyXeReZ@users.noreply.github.com>
2022-08-06 21:07:46 +0200

    Add files via upload

---
+++ ejerciciotres.java
@@ -0,0 +1,15 @@
+public class ejerciciotres {
+    public static void main(String[] args) {
+
+        //Creamos el array de tipo String
+        String[] nombre = new String[]{"Dani", "Mario", "Irene"};
+
+        //Recorremos el array y mostramos su contenido
+        for(int i = 0; i < nombre.length; i++) {
+            System.out.print(nombre[i] + ",");
+        }
+
+    }
+
+}
+
