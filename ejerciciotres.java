9d58e6de5b42fce93f2d6cd34d2422584242dae4
DaNyPC <106661035+DaNyXeReZ@users.noreply.github.com>
2022-08-06 21:10:00 +0200

    Update ejerciciotres.java

--- ejerciciotres.java
+++ ejerciciotres.java
@@ -1,3 +1,6 @@
+//En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
+//Tened en cuenta que los textos pueden venir de un array de tipo String
+
 public class ejerciciotres {
     public static void main(String[] args) {
 
